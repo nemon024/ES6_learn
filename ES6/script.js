74ca8ea3d80085ad80409f288551319932c714f6
naman <namankhetwal99@gmail.com>
2025-04-05 11:07:20 +0530

    Add initial HTML structure and JavaScript variable/function documentation

---
+++ ES6/script.js
@@ -0,0 +1,26 @@
+/*varibles 
+there are 2 typees of varibles 
+1.let:
+@Part of ES6
+@access to inside the block of code only if it is inside the block of code unlike var which is in older version 
+@can be change after ward
+2.const :
+@Part of ES6
+@Create a constant value
+@access to inside the block of code only if it is inside the block of code
+@cant change after declation
+*/
+
+
+/* function
+3 types 
+basic fat arrow
+let a=()=>{} 
+  
+fat arrow function with 1 parameter
+let a = parameter =>{}
+  
+fat arrow function with no return 
+let a = ()=> 122
+console.log(a());
+result will be 122*/
