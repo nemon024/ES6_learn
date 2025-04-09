62c5a6a017ed40ffbeed332064b3b25dba4b8c9d
naman <namankhetwal99@gmail.com>
2025-04-09 13:05:00 +0530

    Add async/await examples and refactor kitchen function for better flow

--- freeCodeCamp/Async.js
+++ freeCodeCamp/Async.js
@@ -1,5 +1,7 @@
 //Asynchronous
 
+const { log } = require("async");
+
 /* settimeout
 setTimeout(()=>{
   console.log("hello")
@@ -125,3 +127,82 @@ let stocks={
   console.log("day ended , shop is closed")
 })
   */
+
+//ASYNC N AWAIT order
+/*let toppingsChoice = () =>{
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        resolve(
+          console.log("which topping do you want"));
+      },3000);
+    }) 
+  }
+
+  async function kitchen(){
+    console.log("A");
+    console.log("B");
+    console.log("C");
+
+    await toppingsChoice()
+    console.log("D");
+    console.log("E");
+
+  }
+  kitchen()
+
+  console.log("cleaning the dishes");
+  console.log("cleaning the tables");
+  console.log("taking the order");
+  
+   */
+  /*let stocks={
+  fruits:["strawberry","graps","banana","apple"],
+  liquid:["water","ice"],
+  holder:["cone","cup","stick"],
+  toppings:["choclate","peanuts"],
+  };
+
+
+  let isShopOpen = true;
+
+  function time(ms){
+    return new Promise((resolve,reject)=>{
+      if(isShopOpen){
+       setTimeout(resolve, ms)
+      }
+      else{
+        reject(console.log("Our shop is closed")
+        );
+      }
+    })
+  }
+
+  async function kitchen() {
+    try{
+      await time(2000)
+      console.log(`${stocks.fruits[0]}`);
+
+      console.log(`Start the production `);
+
+      await time(2000)
+      console.log(`cut the fruit`);
+
+      await time(1000)
+      console.log(`${stocks.liquid[0] } and ${stocks.liquid[1]} is added`)
+    }
+    catch(err){
+      console.log(`Customer left due to ${err}`);
+      
+    }
+    finally{
+      console.log("day ended, shop is closed")
+    }
+  }
+
+  kitchen();
+  
+  
+
+ */
+
+
