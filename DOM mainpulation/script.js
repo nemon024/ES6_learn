80372201066de1f3914303ed6690b13f9ae5e536
naman <namankhetwal99@gmail.com>
2025-04-11 13:12:47 +0530

    Add initial JavaScript DOM course content and structure to script.js and index.html

---
+++ DOM mainpulation/script.js
@@ -0,0 +1,63 @@
+//JavaScript DOM Full Course - GreatStack
+
+/*
+1.what is DOM
+
+2.What is nodes and type of nodes
+
+3.Selecting elements using DOM
+
+  1.getElementById()
+  2.getElementByName()
+  3.getElementByTagName()
+  4.getElementByClassName()
+  5.querySelector() and querySelectorAll()
+  
+4.Traversing elements
+
+  1.selecting parent element
+  2.Selecting child element
+  3.Selecting Sibling elements
+  
+5.Manipulating HTML elements
+  
+  1.createElement()
+  2.appendChild()
+  3.textContent
+  4.innerHTML
+  5.after()
+  6.append()
+  7.prepend()
+  8.insertAdjacentHTML()
+  9.replaceChild()
+  10.cloneNode()
+  11.removeChild()
+  12.insertBefore()
+  
+6.Attribute methods
+
+  1.getAttribute()
+  2.setAttribute()
+  3.hasAttribute()
+  4.removeAttribute()
+  
+7.Manipulating Element's Styles
+
+  1.style property
+  2.cssText
+  3.getComputedStyle()
+  4.className property
+  5.classList property
+  
+8.JavaScript Events
+  
+  1.What is event
+  2.Event Bubbling & Event Captuting
+  3.Event Handler in HTML Attributes
+  4.DOM level 0 event handlers
+  5.addEventListener()
+  6.removeEventListener()
+  7.Event Objects
+  8.Different Types of Event
+  */
+
